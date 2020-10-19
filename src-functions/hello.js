1211a970e20ecfcae44639d2330b6c7fe461ac6e
dragosh <stefanescu.dragos@gmail.com>
2020-10-19 21:01:01 +0300

    initial commit with nfunctiuons

---
+++ src-functions/hello.js
@@ -0,0 +1,9 @@
+exports.handler = async(event, context, callback) => {
+    // your server-side functionality
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Hello world ${Math.floor(Math.random() * 10)}`
+        })
+    });
+};
